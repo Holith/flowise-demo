8eeafbb00709ce0a74cf65e8b77b97bf35ada77b
Holith <portmann.clement@free.fr>
2025-12-02 19:58:13 +0100

    Update CustomerBasket.cs

--- Ordering.API/Application/Models/CustomerBasket.cs
+++ Ordering.API/Application/Models/CustomerBasket.cs
@@ -4,10 +4,12 @@ public class CustomerBasket
 {
     public string BuyerId { get; set; }
     public List<BasketItem> Items { get; set; }
+    public Datetime creationDate { get; set; }
 
     public CustomerBasket(string buyerId, List<BasketItem> items)
     {
         BuyerId = buyerId;
         Items = items;
+        creationDate = DateTime.now;
     }
 }
